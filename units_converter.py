3b648794eb0e7d587d6064087cdad97432671cf6
chorzow <45035276+chorzow@users.noreply.github.com>
2021-02-28 03:40:02 +0300

    Delete units_converter.py

--- units_converter.py
+++
@@ -1,8 +0,0 @@
-# degrees (deg) to radians (rad) and vice-versa
-from math import pi
-value = round(float(input('Enter value to convert:')), ndigits=3)
-units = input('Enter units to convert:')
-if units == 'deg':
-    print(f"{value} deg = {round(value * pi / 180, ndigits=3)} rad")
-elif units == 'rad':
-    print(f"{value} rad = {round(value * 180 / pi, ndigits=3)} deg")
